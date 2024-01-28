589323d5a7bcb60d1e3273c4920c4d65c27d25df
TsilavinaDevJS <tsilavinaandriamahafaly01@gmail.com>
2024-01-28 07:30:29 +0300

    basic usage

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BearBox } from './components/BearBox'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <p>zustand</p>
+      <BearBox/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/stores/bearStore.tsx
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+
+type TBearStoreState = {
+  bears:number,
+  increasePopulation:() => void;
+  removeAllBears: () => void;
+}
+
+// il faudrait typer les donners dans le useBearStore pour le react typescript
+export const useBearStore = create<TBearStoreState>()((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({
+    bears: state.bears + 1
+   })),
+  removeAllBears: () => set({ bears: 0 }),
+}))
